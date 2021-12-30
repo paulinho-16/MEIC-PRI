b802f2821ff98b5a8ca3f5368a21bb75c0a5040e
Pedro <up201809694@fe.up.pt>
2021-12-30 17:15:42 +0000

    Start bootsrap

--- src/app/netflix-system/src/App.js
+++ src/app/netflix-system/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Form, Button } from 'react-bootstrap'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="form-container">
+            <Form className="signup-form">
+                <Form.Group>
+                    <Form.Control className="name-input" type="text" placeholder="name" name="name"></Form.Control>
+                    <Form.Control className="email-input" type="text" placeholder="email" name="email" ></Form.Control>
+                    <Button className="submit-button" value="submit" type="submit">submit</Button>
+                </Form.Group>
+            </Form>
+        </div>
+    )
 }
 
 export default App;
