05c51cf5a57d6a6b08265b7829b66722dfca3054
Pedro <up201809694@fe.up.pt>
2022-01-13 12:40:27 +0000

    Format code

--- src/interface/src/App.js
+++ src/interface/src/App.js
@@ -1,18 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-import Dropdown from 'react-bootstrap/Dropdown';
-import DropdownButton from 'react-bootstrap/DropdownButton';
-import ButtonGroup from 'react-bootstrap/ButtonGroup';
-import NumericInput from 'react-numeric-input';
+import logo from "./logo.svg";
+import "./App.css";
+import Dropdown from "react-bootstrap/Dropdown";
+import DropdownButton from "react-bootstrap/DropdownButton";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
+import NumericInput from "react-numeric-input";
 import DropdownMultiselect from "react-multiselect-dropdown-bootstrap";
-import Multiselect from 'multiselect-react-dropdown';
-import Nav from 'react-bootstrap/Nav'
-import Form from 'react-bootstrap/Form'
-import { Row, Col } from 'react-bootstrap'
+import Multiselect from "multiselect-react-dropdown";
+import Nav from "react-bootstrap/Nav";
+import Form from "react-bootstrap/Form";
+import { Row, Col } from "react-bootstrap";
 // require('./dropdown.js');
-import SearchForm from './components/SearchForm';
-import SearchResults from './components/SearchResults';
-import React from 'react';
+import SearchForm from "./components/SearchForm";
+import SearchResults from "./components/SearchResults";
+import React from "react";
 
 // var state = {
 //     options: [{ name: 'animation', id: 1 }, { name: 'miniSeries', id: 2 }, { name: 'movie', id: 3 }, { name: 'series', id: 4 }, { name: 'short', id: 5 }, { name: 'special', id: 6 }]
@@ -20,19 +20,25 @@ import React from 'react';
 
 class App extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      results: []
+      results: [],
     };
   }
 
   updateResults = (results) => {
-    this.setState({ results: results })
-  }
+    this.setState({ results: results });
+  };
 
   render() {
     return (
-      <div className="App" style={{ background: this.state.results.length > 0 ? 'radial-gradient(#e5e5e5, #ffff, #e5e5e5)' : 'none' }}>
+      <div
+        className="App"
+        style={{
+          background:
+            this.state.results.length > 0? "radial-gradient(#e5e5e5, #ffff, #e5e5e5)" : "none",
+        }}
+      >
         <header className="App-header">
           <h1>Netflix</h1>
         </header>
@@ -40,83 +46,90 @@ class App extends React.Component {
           <SearchForm updateResults={this.updateResults} />
 
           <Form>
-              <Form.Group className="mb-3" controlId="typeSearch">
-                <Form.Label>Type: </Form.Label>
-                {['animation', 'miniSeries', 'movie', 'series', 'short', 'special'].map((type) => (
-                  <div key={type} className="mb-3">
-                    <Form.Check
-                      type="checkbox"
-                      id={`${type}`}
-                      label={`${type}`}
-                    />
-                  </div>
-                ))}
+            <Form.Group className="mb-3" controlId="typeSearch">
+              <Form.Label>Type: </Form.Label>
+              {[
+                "animation",
+                "miniSeries",
+                "movie",
+                "series",
+                "short",
+                "special",
+              ].map((type) => (
+                <div key={type} className="mb-3">
+                  <Form.Check
+                    type="checkbox"
+                    id={`${type}`}
+                    label={`${type}`}
+                  />
+                </div>
+              ))}
+            </Form.Group>
+            <Row className="mb-3">
+              <Form.Group as={Col} controlId="startYearSearchInit">
+                <Form.Label>Start Year: </Form.Label>
+                <NumericInput className="form-control" style={false} min={0} max={3000}/>
               </Form.Group>
-              <Row className="mb-3">
-                <Form.Group as={Col} controlId="startYearSearchInit">
-                  <Form.Label>Start Year: </Form.Label>
-                  <NumericInput className="form-control" style={false} min={0} max={3000} />
-                </Form.Group>
-                TO
-                <Form.Group as={Col} controlId="startYearSearchFinal">
-                  <NumericInput className="form-control" style={false} min={0} max={3000} />
-                </Form.Group>
-              </Row>
+              TO
+              <Form.Group as={Col} controlId="startYearSearchFinal">
+                <NumericInput className="form-control" style={false} min={0} max={3000}/>
+              </Form.Group>
+            </Row>
 
-              <Row className="mb-3">
+            <Row className="mb-3">
               <Form.Group as={Col} controlId="endYearSearchInit">
                 <Form.Label>End Year: </Form.Label>
-                <NumericInput className="form-control" style={false} min={0} max={3000} />
+                <NumericInput className="form-control" style={false} min={0} max={3000}/>
               </Form.Group>
-                TO
+              TO
               <Form.Group as={Col} controlId="endYearSearchInit">
-                <NumericInput className="form-control" style={false} min={0} max={3000} />
+                <NumericInput className="form-control" style={false} min={0} max={3000}/>
               </Form.Group>
-              </Row>
+            </Row>
 
-              <Row className="mb-3">
+            <Row className="mb-3">
               <Form.Group as={Col} controlId="episodesSearchMin">
                 <Form.Label>Number Episodes: </Form.Label>
-                <NumericInput className="form-control" style={false} min={0} max={10000} />
+                <NumericInput className="form-control" style={false} min={0} max={10000}/>
               </Form.Group>
-                TO
+              TO
               <Form.Group as={Col} controlId="episodesSearchMax">
-                <NumericInput className="form-control" style={false} min={0} max={10000} />
+                <NumericInput className="form-control" style={false} min={0} max={10000}/>
               </Form.Group>
-              </Row>
+            </Row>
 
-              <Row className="mb-3">
+            <Row className="mb-3">
               <Form.Group controlId="runtimeSearchMin">
                 <Form.Label>Runtime: </Form.Label>
-                <NumericInput className="form-control" style={false} min={0} max={10000} />
+                <NumericInput className="form-control" style={false} min={0} max={10000}/>
               </Form.Group>
-                TO
+              TO
               <Form.Group controlId="runtimeSearchMax">
-                <NumericInput className="form-control" style={false} min={0} max={10000} />
+                <NumericInput className="form-control" style={false} min={0} max={10000}/>
               </Form.Group>
-              </Row>
+            </Row>
 
-              <Row className="mb-3">
+            <Row className="mb-3">
               <Form.Group as={Col} controlId="ratingSearchMin">
                 <Form.Label>Rating: </Form.Label>
-                <NumericInput className="form-control" style={false} min={0} max={10} />
+                <NumericInput className="form-control" style={false} min={0} max={10}/>
               </Form.Group>
               TO
               <Form.Group as={Col} controlId="ratingSearchMax">
-                <NumericInput className="form-control" style={false} min={0} max={10} />
+                <NumericInput className="form-control" style={false} min={0} max={10}/>
               </Form.Group>
-              </Row>
+            </Row>
 
-              <Row className="mb-3">
+            <Row className="mb-3">
               <Form.Group as={Col} controlId="numVotesSearchMin">
                 <Form.Label>Num Votes: </Form.Label>
-                <NumericInput className="form-control" style={false} min={0} max={10000000} />
+                <NumericInput className="form-control" style={false} min={0} max={10000000}/>
               </Form.Group>
-                TO
+              TO
               <Form.Group as={Col} controlId="numVotesSearchMax">
-                <NumericInput className="form-control" style={false} min={0} max={10000000} />
+                <NumericInput className="form-control" style={false} min={0} max={10000000}/>
               </Form.Group>
-              </Row>
+            </Row>
           </Form>
 
           {/* TODO: Replace by glass icon */}
